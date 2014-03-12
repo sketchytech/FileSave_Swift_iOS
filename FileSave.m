7d5221e729e6afd51e94e66e055ce2f10e1144aa
Anthony Levings <sketchytech@gylphi.co.uk>
2014-03-12 09:29:26 +0000

    Update FileSave.m
    Added methods for saving files to Application Support, Temporary and Caches directories

--- FileSave.m
+++ FileSave.m
@@ -9,47 +9,9 @@
 
 @implementation FileSave
 
+#pragma mark - public methods
 
-
-+ (NSURL *)applicationDocumentsDirectory {
-    
-    
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
-                                                   inDomains:NSUserDomainMask] lastObject];
-}
-
-+ (NSURL *)applicationLibraryDirectory {
-    
-    
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSLibraryDirectory
-                                                   inDomains:NSUserDomainMask] lastObject];
-}
-
-+(NSString *)stripSlashIfNeeded:(NSString *)stringWithPossibleSlash {
-    
-    // If the file name contains a slash at the beginning then we remove so that we don't end up with two
-    if ([stringWithPossibleSlash compare:@"/" options:NSLiteralSearch range:NSMakeRange(0, 1)]==NSOrderedSame) {
-        stringWithPossibleSlash = [stringWithPossibleSlash substringFromIndex:1];
-    }
-    // Return the string with no slash at the beginning
-    return stringWithPossibleSlash;
-}
-
-+(BOOL)createSubDirectory:(NSString *)subdirectoryPath {
-    NSError *error;
-    BOOL isDir;
-    BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:subdirectoryPath isDirectory:&isDir];
-    if (exists) {
-        /* a file of the same name exists, we don't care about this so won't do anything */
-        if (isDir) {
-            /* subdirectory already exists, don't create it again */
-            return YES;
-        }
-    }
-    BOOL success = [[NSFileManager defaultManager] createDirectoryAtPath:subdirectoryPath withIntermediateDirectories:YES attributes:nil error:&error];
-    if (error) NSLog(@"%@",error);
-    return success;
-}
+#pragma mark - data methods
 
 +(BOOL)saveDataToDocumentsDirectory:(NSData *)fileData withName:(NSString *)path andSubDirectory:(NSString *)subdirectory
 {
@@ -105,6 +67,101 @@
     return ok;
 }
 
++(BOOL)saveDataToTemporaryDirectory:(NSData *)fileData withName:(NSString *)path andSubDirectory:(NSString *)subdirectory
+{
+    
+    // Remove unnecessary slash if need
+    path = [self stripSlashIfNeeded:path];
+    subdirectory = [self stripSlashIfNeeded:subdirectory];
+    
+    // Create generic beginning to file save path
+    NSMutableString *savePath = [[NSMutableString alloc] initWithFormat:@"%@/",[self applicationTemporaryDirectory].path];
+    
+    
+    if (subdirectory){
+        [savePath appendString:subdirectory];
+        [self createSubDirectory:[savePath copy]];
+        [savePath appendString:@"/"];
+    }
+    
+    
+    // Add requested save path
+    [savePath appendString:path];
+    
+    NSLog(@"%@",savePath);
+    // Save the file and see if it was successful
+    BOOL ok = [[NSFileManager defaultManager] createFileAtPath:[savePath copy] contents:fileData attributes:nil];
+    
+    // Return status of file save
+    return ok;
+}
+
++(BOOL)saveDataToCachesDirectory:(NSData *)fileData withName:(NSString *)path andSubDirectory:(NSString *)subdirectory
+{
+    
+    // Remove unnecessary slash if need
+    path = [self stripSlashIfNeeded:path];
+    subdirectory = [self stripSlashIfNeeded:subdirectory];
+    
+    // Create generic beginning to file save path
+    NSMutableString *savePath = [[NSMutableString alloc] initWithFormat:@"%@/",[self applicationCachesDirectory].path];
+    
+    
+    if (subdirectory){
+        [savePath appendString:subdirectory];
+        [self createSubDirectory:[savePath copy]];
+        [savePath appendString:@"/"];
+    }
+    
+    
+    // Add requested save path
+    [savePath appendString:path];
+    
+    NSLog(@"%@",savePath);
+    // Save the file and see if it was successful
+    BOOL ok = [[NSFileManager defaultManager] createFileAtPath:[savePath copy] contents:fileData attributes:nil];
+    
+    // Return status of file save
+    return ok;
+}
+
++(BOOL)saveDataToApplicationSupportDirectory:(NSData *)fileData withName:(NSString *)path andSubDirectory:(NSString *)subdirectory excludeFromBackup:(BOOL)exclude
+{
+    
+    // Remove unnecessary slash if need
+    path = [self stripSlashIfNeeded:path];
+    subdirectory = [self stripSlashIfNeeded:subdirectory];
+    
+    // Create generic beginning to file save path
+    NSMutableString *savePath = [[NSMutableString alloc] initWithFormat:@"%@/",[self applicationSupportDirectory].path];
+    
+    
+    if (subdirectory){
+        [savePath appendString:subdirectory];
+        [self createSubDirectory:[savePath copy]];
+        [savePath appendString:@"/"];
+    }
+    
+    
+    // Add requested save path
+    [savePath appendString:path];
+    
+    NSLog(@"%@",savePath);
+    
+    NSError *error;
+    // Save the file and see if it was successful
+    BOOL ok = [[NSFileManager defaultManager] createFileAtPath:[savePath copy] contents:fileData attributes:nil];
+    if (exclude) { [[NSURL URLWithString:savePath] setResourceValue:@YES
+                                                               forKey:NSURLIsExcludedFromBackupKey
+                                                                error:&error];}
+    if (error) NSLog(@"%@", error);
+    
+    // Return status of file save
+    return ok;
+}
+
+#pragma mark - string methods
+
 +(BOOL)saveContentsOfStringToDocumentsDirectory:(NSString *)fileString withName:(NSString *)path andSubDirectory:(NSString *)subdirectory {
     // Remove unnecessary slash if need
     path = [self stripSlashIfNeeded:path];
@@ -157,5 +214,158 @@
     return ok;
 
 }
++(BOOL)saveContentsOfStringToTemporaryDirectory:(NSString *)fileString withName:(NSString *)path andSubDirectory:(NSString *)subdirectory {
+    
+    // Remove unnecessary slash if need
+    path = [self stripSlashIfNeeded:path];
+    
+    // Create generic beginning to file save path
+    NSMutableString *savePath = [[NSMutableString alloc] initWithFormat:@"%@/",[self applicationTemporaryDirectory].path];
+    
+    if (subdirectory){
+        [savePath appendString:subdirectory];
+        [self createSubDirectory:[savePath copy]];
+        [savePath appendString:@"/"];
+    }
+    
+    // Add requested save path
+    [savePath appendString:path];
+    
+    NSError *error;
+    // Save the file and see if it was successful
+    BOOL ok = [fileString writeToFile:savePath atomically:NO encoding:NSUTF8StringEncoding error:&error];
+    
+    if (error) NSLog(@"%@", error);
+    
+    // Return status of file save
+    return ok;
+    
+}
+
++(BOOL)saveContentsOfStringToCachesDirectory:(NSString *)fileString withName:(NSString *)path andSubDirectory:(NSString *)subdirectory {
+    
+    // Remove unnecessary slash if need
+    path = [self stripSlashIfNeeded:path];
+    
+    // Create generic beginning to file save path
+    NSMutableString *savePath = [[NSMutableString alloc] initWithFormat:@"%@/",[self applicationCachesDirectory].path];
+    
+    if (subdirectory){
+        [savePath appendString:subdirectory];
+        [self createSubDirectory:[savePath copy]];
+        [savePath appendString:@"/"];
+    }
+    
+    // Add requested save path
+    [savePath appendString:path];
+    
+    NSError *error;
+    // Save the file and see if it was successful
+    BOOL ok = [fileString writeToFile:savePath atomically:NO encoding:NSUTF8StringEncoding error:&error];
+    
+    if (error) NSLog(@"%@", error);
+    
+    // Return status of file save
+    return ok;
+    
+}
+
+
++(BOOL)saveContentsOfStringToApplicationSupportDirectory:(NSString *)fileString withName:(NSString *)path andSubDirectory:(NSString *)subdirectory excludeFromBackUp:(BOOL)exclude {
+    
+    // Remove unnecessary slash if need
+    path = [self stripSlashIfNeeded:path];
+    
+    // Create generic beginning to file save path
+    NSMutableString *savePath = [[NSMutableString alloc] initWithFormat:@"%@/",[self applicationSupportDirectory].path];
+    
+    if (subdirectory){
+        [savePath appendString:subdirectory];
+        [self createSubDirectory:[savePath copy]];
+        [savePath appendString:@"/"];
+    }
+    
+    // Add requested save path
+    [savePath appendString:path];
+    
+    NSError *error;
+    // Save the file and see if it was successful
+    BOOL ok = [fileString writeToFile:savePath atomically:NO encoding:NSUTF8StringEncoding error:&error];
+    
+    if (error) NSLog(@"%@", error);
+    
+    
+    if (exclude) { [[NSURL URLWithString:savePath] setResourceValue:@YES
+                                        forKey:NSURLIsExcludedFromBackupKey
+                                                                error:&error];}
+    if (error) NSLog(@"%@", error);
+    // Return status of file save
+    return ok;
+    
+}
+#pragma mark - private methods
+
+#pragma mark - directories
+
++ (NSURL *)applicationDocumentsDirectory {
+    
+    
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
+                                                   inDomains:NSUserDomainMask] lastObject];
+}
+
++ (NSURL *)applicationLibraryDirectory {
+    
+    
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSLibraryDirectory
+                                                   inDomains:NSUserDomainMask] lastObject];
+}
+
++(NSURL *)applicationSupportDirectory {
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSApplicationSupportDirectory
+                                                   inDomains:NSUserDomainMask] lastObject];
+}
+
+
++(NSURL *)applicationTemporaryDirectory {
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSApplicationSupportDirectory
+                                                   inDomains:NSUserDomainMask] lastObject];
+}
+
++(NSURL *)applicationCachesDirectory {
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSApplicationSupportDirectory
+                                                   inDomains:NSUserDomainMask] lastObject];
+}
+
+#pragma mark - strip slashes
+
++(NSString *)stripSlashIfNeeded:(NSString *)stringWithPossibleSlash {
+    
+    // If the file name contains a slash at the beginning then we remove so that we don't end up with two
+    if ([stringWithPossibleSlash compare:@"/" options:NSLiteralSearch range:NSMakeRange(0, 1)]==NSOrderedSame) {
+        stringWithPossibleSlash = [stringWithPossibleSlash substringFromIndex:1];
+    }
+    // Return the string with no slash at the beginning
+    return stringWithPossibleSlash;
+}
+
++(BOOL)createSubDirectory:(NSString *)subdirectoryPath {
+    NSError *error;
+    BOOL isDir;
+    BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:subdirectoryPath isDirectory:&isDir];
+    if (exists) {
+        /* a file of the same name exists, we don't care about this so won't do anything */
+        if (isDir) {
+            /* subdirectory already exists, don't create it again */
+            return YES;
+        }
+    }
+    BOOL success = [[NSFileManager defaultManager] createDirectoryAtPath:subdirectoryPath withIntermediateDirectories:YES attributes:nil error:&error];
+    
+    if (error) NSLog(@"%@",error);
+    
+    return success;
+}
+
 
 @end
